7f7861554def1e262ad5b054f3b9881b679a0815
EyupCanbay <eyupcanbay40@gmail.com>
2025-05-07 17:17:57 +0300

    main test process added

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbSource = "postgresql://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *pgxpool.Pool
+
+// TestMain initializes the database connection and sets up the queries object for testing
+func TestMain(m *testing.M) {
+	// creating connection database pool
+	testDB, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
+	}
+	defer testDB.Close()
+
+	// creating query object
+	testQueries = New(testDB)
+
+	// run test
+	os.Exit(m.Run())
+}
