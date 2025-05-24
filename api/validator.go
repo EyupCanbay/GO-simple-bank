e0ff721c893b4ee4dc63ac028555045f2516e1f8
EyupCanbay <eyupcanbay40@gmail.com>
2025-05-24 18:43:42 +0300

    Add supported currency validator

---
+++ api/validator.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"simple_bank/utils"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
+	if currency, ok := fieldLevel.Field().Interface().(string); ok {
+		return utils.IsSupportedCurrency(currency)
+	}
+	return false
+}
